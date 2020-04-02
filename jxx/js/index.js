5bfa329e56b05f89742bf10781fa22dc4a4a0ac7
zhangguowei-users <zhangguowei158@qq.com>
2020-04-02 16:54:58 +0800

    'jxx'

---
+++ jxx/js/index.js
@@ -0,0 +1,33 @@
+var ip = "";
+//是否登录检测
+$("#login").click(function(){
+    window.location.href = "./login.html";
+});
+$(".control").click(function(){
+    $.ajax({
+        url:ip + "",
+        type: "POST",
+        data: "requestSession",
+        success: function(data){
+            if(data == "" || data == null){
+                window.location.href = "./login.html";
+            }else if(this.className == "control one"){
+                window.location.href = "./welcome.html";                
+            }else if(this.className == "control two"){
+                window.location.href = "./welcome.html";
+            }else if(this.className == "control three"){
+                window.location.href = "./welcome.html";
+            }else if(this.className =="control four"){
+                window.location.href = "./welcome.html";
+            };
+        }
+    });
+});
+$("#log").click(function(){
+    var value = "key";
+    sessionStorage.setItem("key",value);
+});
+$("#del").click(function(){
+    sessionStorage.removeItem("key");
+    
+});
