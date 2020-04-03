5ff9d5d702669a14b5dcbde2db31bf2f46292f37
zhangguowei-users <zhangguowei158@qq.com>
2020-04-03 13:27:55 +0800

    第四次提交

--- jxx/js/index.js
+++ jxx/js/index.js
@@ -1,33 +1,34 @@
-var ip = "";
 //是否登录检测
-$("#login").click(function(){
-    window.location.href = "./login.html";
-});
-$(".control").click(function(){
+var ip = "";
+setInterval(function () {
     $.ajax({
         url:ip + "",
-
         type: "POST",
-        data: "requestSession",
-        success: function(data){
-            if(data == "" || data == null){
-                window.location.href = "./login.html";
-            }else if(this.className == "control one"){
-                window.location.href = "./welcome.html";                
-            }else if(this.className == "control two"){
-                window.location.href = "./welcome.html";
-            }else if(this.className == "control three"){
-                window.location.href = "./welcome.html";
-            }else if(this.className =="control four"){
-                window.location.href = "./welcome.html";
+        data: "{cmd:requestSession}",
+        success:function(data) {
+            if (data.session == success) {
+                $("#login").html("已登录");
+                $(".control").click(function () {
+                    if (data.session == "" || data == null) {
+                        window.location.href = "./login.html";
+                    } else if (this.className == "control one") {
+                        window.location.href = "./welcome.html";
+                    } else if (this.className == "control two") {
+                        window.location.href = "./welcome.html";
+                    } else if (this.className == "control three") {
+                        window.location.href = "./welcome.html";
+                    } else if (this.className == "control four") {
+                        window.location.href = "./welcome.html";
+                    }
+                });
+            }else{
+                $("#login").click(function () {
+                    window.location.href = "./login.html";
+                });
+                $(".control").click(function(){
+                    window.location.href = "./login.html";
+                });
             };
         }
     });
-});
-$("#log").click(function(){
-    var value = "key";
-    sessionStorage.setItem("key",value);
-});
-$("#del").click(function(){
-    sessionStorage.removeItem("key");
-});
+},10000);
