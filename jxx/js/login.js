e8209b900299eb7420d7af11b36e96d5dd19f487
zhangguowei-users <zhangguowei158@qq.com>
2020-04-03 15:41:28 +0800

    第8次提交

--- jxx/js/login.js
+++ jxx/js/login.js
@@ -1,30 +1,30 @@
 $(function name(){
 $(document).click(function(){
-    $(".name").css("background","url(./img/login__03.png) no-repeat");
+        $(".name").css("background","url(./img/login__03.png) no-repeat");
 });
 $(".name").click(function(event){
-    $(".name").css("background","url(./img/login__05.png) no-repeat");
-    event.stopPropagation();
+        $(".name").css("background","url(./img/login__05.png) no-repeat");
+        event.stopPropagation();
 });
 });
 $(function value(){
     $(".value").click(function(event){
-            event.stopPropagation();
-            $(".value").css("background","url(./img/login__10.png) no-repeat")
+        event.stopPropagation();
+        $(".value").css("background","url(./img/login__10.png) no-repeat")
     });
     $(document).click(function(){
         $(".value").css("background","url(./img/login__09.png) no-repeat");
     });
 });
 $("#input").mouseover(function () {
-    $(this).css({"background":"url(./img/login__16.png) no-repeat","background-size":"350px 45px"});
+        $(this).css({"background":"url(./img/login__16.png) no-repeat","background-size":"350px 45px"});
 });
 $("#input").mouseout(function(){
-    $(this).css({"background":"url(./img/login__14.png) no-repeat","background-size":"350px 45px"});
+        $(this).css({"background":"url(./img/login__14.png) no-repeat","background-size":"350px 45px"});
 });
 $("#input").click(function () {
-    var name = $("#name").html();
-    var value = $("#value").html();
+       var name = $("#name").html();
+       var value = $("#value").html();
    $.ajax({
        url:config.ip + "",
        type: "POST",
