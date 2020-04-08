c2d56e8b20f01ef1ee0facb01bd0bc46f23d3bd1
zhangguowei-users <zhangguowei158@qq.com>
2020-04-08 16:57:07 +0800

    第18次提交

--- jxx/js/welcome.js
+++ jxx/js/welcome.js
@@ -34,3 +34,28 @@ $(".five").click(function () {
     $(".dh-bottom").css("display","none");
     $(".five5").css("display","inline-block");
 });
+$(".eone").click(function(){
+    var height = $(".rone").height();
+    if(height == 0 ){
+        $(".rone").animate({
+            height:'300px'
+        })
+    }else{
+        $(".rone").animate({
+            height:'0px'
+        })
+    }
+});
+$(".etwo").click(function(){
+    var height = $(".rtwo").height();
+    if(height == 0 ){
+        $(".rtwo").animate({
+            height:'300px'
+        })
+    }else{
+        $(".rtwo").animate({
+            height:'0px'
+        })
+    }
+
+});
