c26a731cf7b4ccd0a04ae88a667dcfb3018d2965
zhangguowei-users <zhangguowei158@qq.com>
2020-04-14 15:18:16 +0800

    32

--- jxx/js/index.js
+++ jxx/js/index.js
@@ -4,22 +4,31 @@ setInterval(function () {
         url:config.ip +config.port+ "/getUserInfo",
         type: "POST",
         xhrFields:{withCredentials:true},
-        success:function(data) {
+        success:function(data){
+            console.log(dat);
             if (data.length > 0) {
                 $("#login").css("display","none");
                 $(".control").click(function () {
                     if (this.className == "control one") {
                         window.location.href = "./welcome.html";
                         //按className操作进入后的页面
+                        $(".dh-bottom").css("display","none");
+                        $(".two2").css("display","inline-block");
                     } else if (this.className == "control two") {
                         window.location.href = "./welcome.html";
                         //按className操作进入后的页面
+                        $(".dh-bottom").css("display","none");
+                        $(".three3").css("display","inline-block");
                     } else if (this.className == "control three") {
                         window.location.href = "./welcome.html";
                         //按className操作进入后的页面
+                        $(".dh-bottom").css("display","none");
+                        $(".four4").css("display","inline-block");
                     } else if (this.className == "control four") {
                         window.location.href = "./welcome.html";
                         //按className操作进入后的页面
+                        $(".dh-bottom").css("display","none");
+                        $(".five5").css("display","inline-block");
                     }
                 });
             }else{
