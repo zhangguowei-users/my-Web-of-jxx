8010053dfa7219b4f65445cc0bbce3dbcd95b41d
zhangguowei-users <zhangguowei158@qq.com>
2020-04-15 16:56:19 +0800

    zangguowei123

--- jxx/js/zygx.js
+++ jxx/js/zygx.js
@@ -2,29 +2,41 @@ $(document).ready(function(){
     dengluLocation();
     $("#inf-namenow-time").html(newTime());
     tiaozhuan();
-    $(".eone").click(function(){
-        var height = $(".rone").height();
-        if(height == 0 ){
-            $(".rone").animate({
-                height:'300px'
-            })
-        }else{
-            $(".rone").animate({
-                height:'0px'
-            })
+    // $(".eone").click(function(){
+    //     var height = $(".rone").height();
+    //     if(height == 0 ){
+    //         $(".rone").animate({
+    //             height:'300px'
+    //         })
+    //     }else{
+    //         $(".rone").animate({
+    //             height:'0px'
+    //         })
+    //     }
+    // });
+    // $(".etwo").click(function(){
+    //     var height = $(".rtwo").height();
+    //     if(height == 0 ){
+    //         $(".rtwo").animate({
+    //             height:'300px'
+    //         })
+    //     }else{
+    //         $(".rtwo").animate({
+    //             height:'0px'
+    //         })
+    //     }
+    // });
+    $.ajax({
+        url:config.ip + config.port + '/getUserInfo',
+        type: 'POST',
+        async: false,
+        xhrFields:{withCredentials:true},
+        success:function(data){
+            if(data.length > 0){call = true};
+            if(data.length <= 0){call = false};
+        },
+        error:function(){
+            call = false;
         }
-    });
-    $(".etwo").click(function(){
-        var height = $(".rtwo").height();
-        if(height == 0 ){
-            $(".rtwo").animate({
-                height:'300px'
-            })
-        }else{
-            $(".rtwo").animate({
-                height:'0px'
-            })
-        }
-    
     });
     });
