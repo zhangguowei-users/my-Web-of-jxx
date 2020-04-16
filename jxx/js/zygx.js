7481539e92a513e9f3830784d1b86320a2653fa2
zhangguowei-users <zhangguowei158@qq.com>
2020-04-16 15:31:12 +0800

    zhangguowei MaxHeight

--- jxx/js/zygx.js
+++ jxx/js/zygx.js
@@ -10,17 +10,21 @@ $(document).ready(function(){
         xhrFields:{withCredentials:true},
         success:function(data){
                 var n=0;
+                var gloArr = [];
                 for(key in data){
                    $(".qone").append(`<div class="cds eone ${n}">${data[key].menuename}</div><div class="sfq rone"></div>`);
                      if(data[key].subMenue.length != 0){
                         for(k in data[key].subMenue){
-                           $(`.${n}`).next().append("<div class='sfqx'>" + data[key].subMenue[k].menuename + "</div>");
+                           $(`.${n}`).next().append(`<div class='sfqx'>${data[key].subMenue[k].menuename}</div>`);
+                           var Num = $(".sfqx").length;
+                           gloArr.push(Num);
                         };
                      };       
                     n++;
                 };
+                var MaxHeight = Math.max.apply(null,gloArr);
                 //手风琴事件
-                shoufengqin(".eone");
+                shoufengqin(".eone",MaxHeight);
                 //滑块移动事件
                 huakuaiMove(".eone");
                 //点击变色事件
