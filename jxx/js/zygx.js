f27d475ba405661265144c0af6d3deaf73a48ffc
zhangguowei-users <zhangguowei158@qq.com>
2020-04-16 15:49:15 +0800

    zhangguowei

--- jxx/js/zygx.js
+++ jxx/js/zygx.js
@@ -11,6 +11,7 @@ $(document).ready(function(){
         success:function(data){
                 var n=0;
                 var gloArr = [];
+                var Divheight = [];
                 for(key in data){
                    $(".qone").append(`<div class="cds eone ${n}">${data[key].menuename}</div><div class="sfq rone"></div>`);
                      if(data[key].subMenue.length != 0){
@@ -18,11 +19,16 @@ $(document).ready(function(){
                            $(`.${n}`).next().append(`<div class='sfqx'>${data[key].subMenue[k].menuename}</div>`);
                            var Num = $(".sfqx").length;
                            gloArr.push(Num);
+                           var vheight = $(".sfqx").height();
+                           Divheight.push(vheight); 
                         };
                      };       
                     n++;
                 };
-                var MaxHeight = Math.max.apply(null,gloArr);
+                console.log(gloArr,Divheight)
+                var MaxDivheight = Math.max.apply(null,Divheight);
+                var MaxHeight = Math.max.apply(null,gloArr) * MaxDivheight;
+                console.log(MaxHeight,MaxDivheight);
                 //手风琴事件
                 shoufengqin(".eone",MaxHeight);
                 //滑块移动事件
