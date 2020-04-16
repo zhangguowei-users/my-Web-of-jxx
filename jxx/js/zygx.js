c7f84ca09c5a2e4d022eb025efa08c25064024e7
zhangguowei-users <zhangguowei158@qq.com>
2020-04-16 10:49:13 +0800

    张国伟 55

--- jxx/js/zygx.js
+++ jxx/js/zygx.js
@@ -3,41 +3,42 @@ $(document).ready(function(){
     $("#inf-namenow-time").html(newTime());
     tiaozhuan();
     huoquName();
-    // $(".eone").click(function(){
-    //     var height = $(".rone").height();
-    //     if(height == 0 ){
-    //         $(".rone").animate({
-    //             height:'300px'
-    //         })
-    //     }else{
-    //         $(".rone").animate({
-    //             height:'0px'
-    //         })
-    //     }
-    // });
-    // $(".etwo").click(function(){
-    //     var height = $(".rtwo").height();
-    //     if(height == 0 ){
-    //         $(".rtwo").animate({
-    //             height:'300px'
-    //         })
-    //     }else{
-    //         $(".rtwo").animate({
-    //             height:'0px'
-    //         })
-    //     }
-    // });
+    
     $.ajax({
-        url:config.ip + config.port + '/getUserInfo',
+        url:config.ip + config.port + '/getMenue',
         type: 'POST',
         async: false,
         xhrFields:{withCredentials:true},
         success:function(data){
-            if(data.length > 0){call = true};
-            if(data.length <= 0){call = false};
-        },
-        error:function(){
-            call = false;
+                console.log(data);
+                var n=0;
+                for(key in data){
+                   $(".qone").append(`<div class="cds eone ${n}">${data[key].menuename}</div><div class="sfq rone"></div>`);
+                     if(data[key].subMenue.length != 0){
+                        for(k in data[key].subMenue){
+                           console.log(data[key].subMenue[k]);
+                           $(`.${n}`).next().append("<div class='sfqx'>" + data[key].subMenue[k].menuename + "</div>");
+                        };
+                     };       
+                    n++;
+                };
+                //手风琴事件
+                $(".eone").click(function(){
+                    var height = $(this).next().height();
+                    if(height == 0 ){
+                        $(this).next().animate({
+                            height:'300px'
+                        })
+                    }else{
+                        $(this).next().animate({
+                            height:'0px'
+                        })
+                    }
+                });
+                //滑块移动事件
+                $(".eone").click(function(){
+                    
+                });
         }
     });
     });
