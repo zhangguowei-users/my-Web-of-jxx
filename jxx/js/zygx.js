4043b7f6714513ffb9aadacea5e36d02ec827be7
zhangguowei-users <zhangguowei158@qq.com>
2020-04-17 15:16:28 +0800

    张国伟 -155

--- jxx/js/zygx.js
+++ jxx/js/zygx.js
@@ -3,13 +3,34 @@ $(document).ready(function(){
     $("#inf-namenow-time").html(newTime());
     tiaozhuan();
     huoquName();
-    $("#browser").treeview();
+    
     $.ajax({
         url:config.ip + config.port + '/getMenue',
         type: 'POST',
         async: false,
         xhrFields:{withCredentials:true},
         success:function(data){
+                //形成树菜单
+               function tree(data){
+                for(key of data){
+                   console.log(key.subMenue.length);
+                  if(key.subMenue.length != 0){
+                     $(".qone").append(`<ul><li class="closed"><span class="folder">${key.menuename}</span></li></ul>`);
+                     for(k of key.subMenue){
+                        if(k.subMenue.length !=0){
+                          $(".closed").append(`<ul><li class="closed"><span class="folder">${k.menuename}</span></li></ul>`)
+                        }else{
+                          $(".closed").append(`<ul><li><span class="file">${k.menuename}</span></li></ul>`)
+                        }
+                        tree(k.subMenue);
+                     };
+                  }else{
+                     $(".qone").append(`<ul><li><span class="file">${key.menuename}</span></li></ul>`);
+                  }
+                }
+               };
+               tree(data);
+               $("#browser").treeview();
                 //滑块移动事件
                 huakuaiMove(".folder");
                 //点击变色事件
