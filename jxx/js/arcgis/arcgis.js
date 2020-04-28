d2b7b60da1c6d8dbdcb67a7e306af2d7428f13b2
wesihun <1498731041@qq.com>
2020-04-28 13:52:58 +0800

    init

--- jxx/js/arcgis/arcgis.js
+++ jxx/js/arcgis/arcgis.js
@@ -175,6 +175,8 @@ function messageBox(feature) {//信息框
     divElement += "<div>" + "数据年份：" + feature.attributes.SJNF    +"</div>"
 
     $(".lone").append(divElement);
+    $(".mone").html(feature.attributes.DLMC);
+
 }
 
 
@@ -189,8 +191,8 @@ function queryDLTB(data, menue){//点击左侧树
             globalQueryClass.queryTask(sql);
         }});
 
-
         $(".bing").css("display","none");
+        creatZhuReport();
 
     }else {
         for(var i=0; i<data.length; i++){
@@ -203,11 +205,113 @@ function queryDLTB(data, menue){//点击左侧树
 
         globalQueryClass.queryTask(sql);
 
-        $(".bing").css("display","inline-block");
-        $(".zhu").css("display","inline-block");
+
+        createBingReport();
+        creatZhuReport()
 
     }
 
+}
+
+function createBingReport()
+{
+    $(".bing").css("display","inline-block");
+
+    var myChart = echarts.init(document.getElementById('bing'));
+    option = {
+        title: {
+            text: '同名数量统计',
+            left: 'center'
+        },
+        tooltip: {
+            trigger: 'item',
+            formatter: '{a} <br/>{b}: {c} ({d}%)'
+        },
+        legend: {
+            orient: 'vertical',
+            left: 10,
+            data: ['直接访问', '邮件营销', '联盟广告', '视频广告', '搜索引擎']
+        },
+        series: [
+            {
+                name: '访问来源',
+                type: 'pie',
+                radius: ['50%', '70%'],
+                avoidLabelOverlap: false,
+                label: {
+                    show: false,
+                    position: 'center'
+                },
+                emphasis: {
+                    label: {
+                        show: true,
+                        fontSize: '30',
+                        fontWeight: 'bold'
+                    }
+                },
+                labelLine: {
+                    show: false
+                },
+                data: [
+                    {value: 335, name: '直接访问'},
+                    {value: 310, name: '邮件营销'},
+                    {value: 234, name: '联盟广告'},
+                    {value: 135, name: '视频广告'},
+                    {value: 1548, name: '搜索引擎'}
+                ]
+            }
+        ]
+    };
+    myChart.setOption(option);
+}
+
+function creatZhuReport()
+{
+    $(".zhu").css("display","inline-block");
+
+    var myChartone = echarts.init(document.getElementById('zhu'));
+    option = {
+        title: {
+            text: '同名数量统计',
+            left: 'center'
+        },
+        color: ['#3398DB'],
+        tooltip: {
+            trigger: 'axis',
+            axisPointer: {            // 坐标轴指示器，坐标轴触发有效
+                type: 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+            }
+        },
+        grid: {
+            left: '3%',
+            right: '4%',
+            bottom: '3%',
+            containLabel: true
+        },
+        xAxis: [
+            {
+                type: 'category',
+                data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+                axisTick: {
+                    alignWithLabel: true
+                }
+            }
+        ],
+        yAxis: [
+            {
+                type: 'value'
+            }
+        ],
+        series: [
+            {
+                name: '直接访问',
+                type: 'bar',
+                barWidth: '60%',
+                data: [10, 52, 200, 334, 390, 330, 220]
+            }
+        ]
+    };
+    myChartone.setOption(option);
 }
 
 
