a7a1c1192acb3d64b95d17e20e2d473fc06625cd
zhangguowei-users <zhangguowei158@qq.com>
2020-04-28 14:12:44 +0800

    的撒旦撒旦阿萨阿迪斯

--- jxx/js/index.js
+++ jxx/js/index.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
 if(jiancelogin()){
     $("#login1").css("display","none");
+    $("#gb").css("display","inline-block");
 };
 $("#login1").click(function () {
     window.location.href = "./login.html";
