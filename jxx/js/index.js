bd45b56455df2953ba4dc4df27f582d4d27d11ca
zhangguowei-users <zhangguowei158@qq.com>
2020-04-28 14:37:24 +0800

    dadasdasdas

--- jxx/js/index.js
+++ jxx/js/index.js
@@ -4,6 +4,9 @@ if(jiancelogin()){
     $("#gb").css("display","inline-block");
     $("#help").css("display","inline-block");
     $("#setting").css("display","inline-block");
+    $("#inf-namenow-time").html(newTime());
+    huoquName();
+    $("#login").css("display","inline-block");
 };
 $("#login1").click(function () {
     window.location.href = "./login.html";
