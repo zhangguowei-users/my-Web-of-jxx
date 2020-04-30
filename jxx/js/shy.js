e09d2a88198141ad2c699a3e609d826e80ccf23d
zhangguowei-users <zhangguowei158@qq.com>
2020-04-30 11:00:51 +0800

    dsfsdfsdf

--- jxx/js/shy.js
+++ jxx/js/shy.js
@@ -4,4 +4,21 @@ $(document).ready(function(){
     $("#inf-namenow-time").html(newTime());
     tiaozhuan();
     huoquName();
+    $.ajax({
+        url:config.ip + config.port + '/getMenue',
+        type: 'POST',
+        async: false,
+        xhrFields:{withCredentials:true},
+        success:function(data){
+               //形成树菜单
+               tree(data,".qtwo");
+               $("#browser").treeview();
+                //滑块移动事件
+               huakuaiMove(".folder");
+                //点击变色事件
+               caidanChangeColor(".file");
+                //点击查询
+               queryCd(".ftwo",".stwo","#browser",data);
+        }
+    });
     });
