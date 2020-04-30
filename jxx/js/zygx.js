fd99e3af382d0882722ee32e0711ba1b22aad78e
zhangguowei-users <zhangguowei158@qq.com>
2020-04-30 16:17:32 +0800

    dsadsadsadsa

--- jxx/js/zygx.js
+++ jxx/js/zygx.js
@@ -28,97 +28,6 @@ $(document).ready(function(){
     $("#gb-p1").click(function(){
       $(".theone").css("display","none");
     });
-    /*var myChart = echarts.init(document.getElementById('bing'));
-    option = {
-      title: {
-        text: '同名数量统计',
-        left: 'center'
-       },
-      tooltip: {
-          trigger: 'item',
-          formatter: '{a} <br/>{b}: {c} ({d}%)'
-      },
-      legend: {
-          orient: 'vertical',
-          left: 10,
-          data: ['直接访问', '邮件营销', '联盟广告', '视频广告', '搜索引擎']
-      },
-      series: [
-          {
-              name: '访问来源',
-              type: 'pie',
-              radius: ['50%', '70%'],
-              avoidLabelOverlap: false,
-              label: {
-                  show: false,
-                  position: 'center'
-              },
-              emphasis: {
-                  label: {
-                      show: true,
-                      fontSize: '30',
-                      fontWeight: 'bold'
-                  }
-              },
-              labelLine: {
-                  show: false
-              },
-              data: [
-                  {value: 335, name: '直接访问'},
-                  {value: 310, name: '邮件营销'},
-                  {value: 234, name: '联盟广告'},
-                  {value: 135, name: '视频广告'},
-                  {value: 1548, name: '搜索引擎'}
-              ]
-          }
-      ]
-  };
-  myChart.setOption(option);*/
-  /*var myChartone = echarts.init(document.getElementById('zhu'));
-  option = {
-    title: {
-      text: '同名数量统计',
-      left: 'center'
-     },
-    color: ['#3398DB'],
-    tooltip: {
-        trigger: 'axis',
-        axisPointer: {            // 坐标轴指示器，坐标轴触发有效
-            type: 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
-        }
-    },
-    grid: {
-        left: '3%',
-        right: '4%',
-        bottom: '3%',
-        containLabel: true
-    },
-    xAxis: [
-        {
-            type: 'category',
-            data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
-            axisTick: {
-                alignWithLabel: true
-            }
-        }
-    ],
-    yAxis: [
-        {
-            type: 'value'
-        }
-    ],
-    series: [
-        {
-            name: '直接访问',
-            type: 'bar',
-            barWidth: '60%',
-            data: [10, 52, 200, 334, 390, 330, 220]
-        }
-    ]
-};
-myChartone.setOption(option);*/
-
-
 $("#gb-bing").click(function(){
   $(".bing").css("display","none");
 });
@@ -126,4 +35,23 @@ $("#gb-zhu").click(function(){
   $(".zhu").css("display","none");
 });
 tishi();
+$.ajax({
+    url:config.ip + config.port + '/getAdministration',
+    type: 'POST',
+    async: false,
+    xhrFields:{withCredentials:true},
+    success:function(data){
+        function bianliDF(data){
+        for(var i=0;i<data.length;i++){
+            if(data[i].subAdministrations == null && data[i].subAdministrations.length == 0){
+                $(".xz").append(`<option value="${data[i]}">${data[i].name}</option>`);
+            }else{
+                $(".xz").append(`<option value="${data[i]}">${data[i].name}</option>`);
+                bianliDF(data[i].subAdministrations);
+            };
+        }
+    };
+    bianliDF(data);
+}
+});
 });
