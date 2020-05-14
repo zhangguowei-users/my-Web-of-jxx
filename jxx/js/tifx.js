6a32d5ce02849b205fc2e2617d6fceb31554d310
zhangguowei-users <zhangguowei158@qq.com>
2020-05-14 09:11:04 +0800

    12132131

--- jxx/js/tifx.js
+++ jxx/js/tifx.js
@@ -23,6 +23,35 @@ $(document).ready(function(){
                queryCd(".ftwo",".stwo","#browser",data);
         }
     });
+    //滑块
+    $( function() {
+      $( "#slider" ).slider({
+        value:2009,
+        min:2009,
+        max:2019,
+        step:1,
+        slide: function(event, ui){
+          $( "#amount" ).val(ui.value);
+          console.log(ui.value);
+        }
+      });
+      $("#amount").val($("#slider").slider("value"));
+      console.log($("#slider").slider("value"));
+    });
+    $( function() {
+      $( "#slider1" ).slider({
+        value:2012,
+        min:2009,
+        max:2019,
+        step:1,
+        slide: function(event, ui){
+          $( "#amount1" ).val(ui.value);
+          console.log(ui.value);
+        }
+      });
+      $("#amount1").val($("#slider1").slider("value"));
+      console.log($("#slider1").slider("value"));
+    });
     //测试
     $.ajax({
       url:config.ip + config.port + '/getAdministration',
