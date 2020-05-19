478dcf76caa93be9178162fa614360cc7d8866fc
zhangguowei-users <zhangguowei158@qq.com>
2020-05-19 13:48:38 +0800

    123

--- jxx/js/arcgis/arcgis_report.js
+++ jxx/js/arcgis/arcgis_report.js
@@ -23,7 +23,7 @@ function createAllDLBingReport() {//所有地类饼形图
         option = {
             title: {text: right.name + '所有地类比例报表', left: 'center'}, tooltip: {trigger: 'item', formatter: '{a} <br/>{b} : {c} ({d}%)'},
             legend: {left:1, top:21, orient: 'vertical', data: eval(legendData)},
-            series: [{name: right.name, type: 'pie', radius: '55%', center: ['80%', '50%'], data: eval(seriesData), emphasis: {itemStyle: {shadowBlur: 10, shadowOffsetX: 0, shadowColor: 'rgba(0, 0, 0, 0.5)'}}}]
+            series: [{name: right.name, type: 'pie', radius: '55%', center: ['80%', '70%'], data: eval(seriesData), emphasis: {itemStyle: {shadowBlur: 10, shadowOffsetX: 0, shadowColor: 'rgba(0, 0, 0, 0.5)'}}}]
         };
 
         myChart_bing.setOption(option);
@@ -56,7 +56,7 @@ function createAllDLZhuReport() {//所有地类柱形图
         const quanbu_zhu = document.querySelector("#quanbu-zhu");
         const myChart_zhu = echarts.init(quanbu_zhu);
 
-        option = {title: {text:right.name +'所有地类面积报表',left: 'center'}, xAxis: {type: 'category', data: eval(xAxisData), axisLabel:{interval:0, rotate:45,}}, yAxis: {type: 'value'},
+        option = {title: {text:right.name +'所有地类面积报表',left: 'center'},tooltip: {trigger: 'axis',axisPointer: {type: 'shadow'}}, xAxis: {type: 'category', data: eval(xAxisData), axisLabel:{interval:0, rotate:45,}}, yAxis: {type: 'value'},
             series: [{data: eval(seriesData), type: 'bar', showBackground: true, backgroundStyle: {color: 'rgba(220, 220, 220, 0.8)'}}]};
 
         myChart_zhu.setOption(option);
