2ab078763bb3cb90e67d4109a85189f43584e166
zhangguowei-users <zhangguowei158@qq.com>
2020-05-25 11:35:41 +0800

    123132123

--- jxx/js/shy.js
+++ jxx/js/shy.js
@@ -25,7 +25,66 @@ $(document).ready(function(){
     $("#gb-p1").bind("click",function(){
         $("#css1").css('display','none');
     });
-    
+    //个人中心数据加载
+    $("#myshenheing").sPage({
+      page:1,//当前页码，必填
+      total:10,//数据总条数，必填
+      pageSize:10,//每页显示多少条数据，默认10条
+      totalTxt:"共{total}条",//数据总条数文字描述，{total}为占位符，默认"共{total}条"
+      showTotal:true,//是否显示总条数，默认关闭：false
+      showSkip:true,//是否显示跳页，默认关闭：false
+      showPN:true,//是否显示上下翻页，默认开启：true
+      prevPage:"上一页",//上翻页文字描述，默认“上一页”
+      nextPage:"下一页",//下翻页文字描述，默认“下一页”
+      backFun:function(page){
+          //点击分页按钮回调函数，返回当前页码
+          console.log(page);
+      }
+    });
+    $("#myyitongguo").sPage({
+      page:1,//当前页码，必填
+      total:20,//数据总条数，必填
+      pageSize:10,//每页显示多少条数据，默认10条
+      totalTxt:"共{total}条",//数据总条数文字描述，{total}为占位符，默认"共{total}条"
+      showTotal:true,//是否显示总条数，默认关闭：false
+      showSkip:true,//是否显示跳页，默认关闭：false
+      showPN:true,//是否显示上下翻页，默认开启：true
+      prevPage:"上一页",//上翻页文字描述，默认“上一页”
+      nextPage:"下一页",//下翻页文字描述，默认“下一页”
+      backFun:function(page){
+          //点击分页按钮回调函数，返回当前页码
+          console.log(page);
+      }
+    });
+    $("#myyixiazai").sPage({
+      page:1,//当前页码，必填
+      total:50,//数据总条数，必填
+      pageSize:10,//每页显示多少条数据，默认10条
+      totalTxt:"共{total}条",//数据总条数文字描述，{total}为占位符，默认"共{total}条"
+      showTotal:true,//是否显示总条数，默认关闭：false
+      showSkip:true,//是否显示跳页，默认关闭：false
+      showPN:true,//是否显示上下翻页，默认开启：true
+      prevPage:"上一页",//上翻页文字描述，默认“上一页”
+      nextPage:"下一页",//下翻页文字描述，默认“下一页”
+      backFun:function(page){
+          //点击分页按钮回调函数，返回当前页码
+          console.log(page);
+      }
+    });
+    //操作个人中心的按钮选项
+    $(".btn-tree1").bind("click",function(){
+      $("#myyixiazai,#myyitongguo,#myshenheing,#shenheing,#yitongguo,#yixiazai").css("display","none");
+      if($(this).html() == "审核中"){
+       $("#myshenheing").css("display","block");
+       $("#shenheing").css("display","table");
+      }else if($(this).html() == "已通过"){
+        $("#myyitongguo").css("display","block");
+        $("#yitongguo").css("display","table");
+      }else if($(this).html() == "已下载"){
+        $("#myyixiazai").css("display","block");
+        $("#yixiazai").css("display","table");
+      }
+    });
     huakuaiMove(".btn-tree");
     $(".btn-tree").click(function(){
        if($(this).html() == "待审核"){
