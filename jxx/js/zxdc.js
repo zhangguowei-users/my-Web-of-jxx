6424a65b34f3c319dc7ff99c36781ca88f223e71
zhangguowei-users <zhangguowei158@qq.com>
2020-05-25 08:40:08 +0800

    1231321

--- jxx/js/zxdc.js
+++ jxx/js/zxdc.js
@@ -16,24 +16,6 @@ $(document).ready(function(){
       $("#quxiao").click(function(){
        $("#css").css("display","none");
       });
-    //   $.ajax({
-    //     url:config.ip + config.port + '/getMenue',
-    //     type: 'POST',
-    //     async: false,
-    //     xhrFields:{withCredentials:true},
-    //     success:function(data){
-    //            //形成树菜单
-    //            tree(data,".qtwo");
-    //            $("#browser").treeview();
-    //             //滑块移动事件
-    //            huakuaiMove(".folder");
-    //             //点击变色事件
-    //            caidanChangeColor(".file");
-    //             //点击查询
-    //            queryCd(".ftwo",".stwo","#browser",data);
-    //     }
-    // });
-    //测试
     $.ajax({
       url:config.newip + config.newport + '/arcgis/SpecialInvestigation/GetResourceTypeList',
       type: 'GET',
