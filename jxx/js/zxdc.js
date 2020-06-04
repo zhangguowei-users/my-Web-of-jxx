cccc4d6d1e46c30316975abef4faec40893ea068
zhangguowei-users <zhangguowei158@qq.com>
2020-06-04 14:09:16 +0800

    132132

--- jxx/js/zxdc.js
+++ jxx/js/zxdc.js
@@ -14,16 +14,6 @@ $(document).ready(function(){
         PDclick();
       });
       $("#time1").html(newTime());
-      $("#quxiao").click(function(){
-       $("#css").css("display","none");
-      });
-    $.ajax({
-      url:config.ip + config.port + '/getAdministration',
-      type: 'POST',
-      async: false,
-      xhrFields:{withCredentials:true},
-      success:function(data){
-        option(data,".xz1");
-      }
-    });
-    });
+    
+    
+});
