30bec649cc73bc1c4a53c6f812d981909974c6bc
wesihun <1498731041@qq.com>
2020-07-17 08:42:50 +0800

    init

--- jxx/js/config.js
+++ jxx/js/config.js
@@ -1,34 +1,34 @@
 //业务服务
 var config;
 config={
-    ip: 'http://192.168.1.109',
+    ip: 'http://192.168.11.235',
     port: ':8081',
-    newip: 'http://192.168.1.107',
+    newip: 'http://192.168.11.237',
     newport:':5050',
 };
 
 //地图服务
 var ARCGISCONFIG={
-    DLTB_Dinamic:"http://192.168.1.109:6080/arcgis/rest/services/jixian/DLTB_Dynamic/MapServer",
-    DLTB_FEATURE:"http://192.168.1.109:6080/arcgis/rest/services/jixian/DLTB/FeatureServer",
-    XZQ_TAG_WITH_MAXSCALE_1_50000:"http://192.168.1.109:6080/arcgis/rest/services/jixian/XZQ_TAG_WITH_MAXSCALE1_50000/MapServer",
-    
+    DLTB_Dinamic:"http://192.168.11.235:6080/arcgis/rest/services/jixian/DLTB_Dynamic/MapServer",
+    DLTB_FEATURE:"http://192.168.11.235:6080/arcgis/rest/services/jixian/DLTB/FeatureServer",
+    XZQ_TAG_WITH_MAXSCALE_1_50000:"http://192.168.11.235:6080/arcgis/rest/services/jixian/XZQ_TAG_WITH_MAXSCALE1_50000_MapServer/MapServer",
+
     QueryLevel: "/0",
     FindTaskLevel: 0,
 
-    IMAGE_LAYER_1:"http://192.168.1.109:6080/arcgis/rest/services/jixian/IMAGE_1/ImageServer",
-    IMAGE_LAYER_2:"http://192.168.1.109:6080/arcgis/rest/services/jixian/IMAGE_2/ImageServer",
-    IMAGE_LAYER_3:"http://192.168.1.109:6080/arcgis/rest/services/jixian/IMAGE_3/ImageServer",
+    IMAGE_LAYER_1:"http://192.168.11.235:6080/arcgis/rest/services/jixian/IMAGE_1/ImageServer",
+    IMAGE_LAYER_2:"http://192.168.11.235:6080/arcgis/rest/services/jixian/IMAGE_2/ImageServer",
+    IMAGE_LAYER_3:"http://192.168.11.235:6080/arcgis/rest/services/jixian/IMAGE_3/ImageServer",
 
-    PRINTTASK:"http://192.168.1.109:6080/arcgis/rest/services/Utilities/PrintingTools/GPServer/Export%20Web%20Map%20Task",
+    PRINTTASK:"http://192.168.11.235:6080/arcgis/rest/services/Utilities/PrintingTools/GPServer/Export%20Web%20Map%20Task",
 
-    ARCSERVER:"192.168.1.109",
+    ARCSERVER:"192.168.11.235",
     ARCSERVERPORT:":6080"
 };
 
 //地理服务
 var GEOSERVER={
-    IP:"http://192.168.1.109",
+    IP:"http://192.168.11.235",
     PORT:":65535"
 }
 
